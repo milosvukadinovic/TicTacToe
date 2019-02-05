e197fb1b65077e5378f774a8623de1b54f620991
milosvukadinovic <milos.vukadinovic11@gmail.com>
2019-02-05 15:22:43 +0100

    Update prompt.rb

--- prompt.rb
+++ prompt.rb
@@ -65,10 +65,17 @@ class Prompt
     def show_info 
         puts "
 
-How To Play (Use letters corresponding to board position)
-        [ Q ] [ W ] [ E ] 
-        [ A ] [ S ] [ D ]
-        [ Z ] [ X ] [ C ]
+How To Play:
+         * After writing down names for both players, the first player is asked to make his move according to diagram bellow.
+
+         * Type the Letter according to the position you wish to play.
+
+         * Diagram will show the move you made and will await the next player's turn.
+
+         * Continue playing untill someone wins or results in a draw. Good luck!
+                           [ Q ] [ W ] [ E ] 
+                           [ A ] [ S ] [ D ]
+                           [ Z ] [ X ] [ C ]
 
 "
     end
